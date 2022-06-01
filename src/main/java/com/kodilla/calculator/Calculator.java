eecf33c744c915050056bf89e4b0fcda578db67c
mkuncewicz <mateusz.kuncewicz@onet.pl>
2022-06-01 16:33:35 +0200

    first commit

---
+++ src/main/java/com/kodilla/calculator/Calculator.java
@@ -0,0 +1,14 @@
+package com.kodilla.calculator;
+
+public class Calculator {
+
+    public void someMethod(){
+        System.out.println("Dziala");
+    }
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.someMethod();
+    }
+
+}
